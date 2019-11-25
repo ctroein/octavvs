733ee8112532845cd9b2b09207c14b7326d89763
Carl Troein <carl@coyne.thep.lu.se>
2019-11-25 16:27:59 +0100

    changed version number and packagning info

--- setup.py
+++ setup.py
@@ -6,23 +6,12 @@ executables = {'preprocessing': 'Preprocessing',
                'mcr_als': 'MCR-ALS',
                'clustering': 'Clustering'}
 
-class PostInstallCommand(install):
-    def run(self):
-        install.run(self)
-        print('Running post-install desktop shortcut setup')
-        from pyshortcuts import make_shortcut
-        proj = 'OCTAVVS '
-        for cmd, nom in executables.items():
-            make_shortcut(os.path.join(self.prefix, 'bin', 'oct_'+cmd),
-                          name=proj+nom, icon='octavvs/icons/preppy.ico',
-                          startmenu=False, terminal=True, desktop=True)
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="octavvs-ctroein",
-    version="0.0.14",
+    name="octavvs",
+    version="0.0.15",
     author="Syahril Siregar, Carl Troein, Michiel Op De Beeck et al.",
     author_email="carl@thep.lu.se",
     description="Open Chemometrics Toolkit for Analysis and Visualization of Vibrational Spectroscopy data",
@@ -47,6 +36,5 @@ setuptools.setup(
         'oct_clustering = octavvs.clustering:main',
         'oct_make_icons = octavvs.make_icons:main'],
     },
-#    cmdclass={'install': PostInstallCommand},
 
 )
