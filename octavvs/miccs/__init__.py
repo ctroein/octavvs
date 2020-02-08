ed035fbbaf7319441de21570aa8c039cf3d0f306
Carl Troein <carl@thep.lu.se>
2020-02-08 22:47:26 +0100

    made exceptiondialog a class

--- octavvs/miccs/__init__.py
+++ octavvs/miccs/__init__.py
@@ -4,3 +4,4 @@ from .opusreader import OpusReader
 from .octavvsapplication import OctavvsMainWindow, run_octavvs_application
 from .norepeatstyle import NoRepeatStyle
 
+from .exceptiondialog import ExceptionDialog
