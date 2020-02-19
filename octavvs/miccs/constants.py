394f741d7efb64940bfc037e7f792db15528a242
Carl Troein <carl@thep.lu.se>
2020-02-20 00:02:44 +0100

    moved parts of visualization out of the preprocessing class

---
+++ octavvs/miccs/constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 17:25:10 2020
+
+@author: carl
+"""
+
+WMIN = 800
+WMAX = 4000
+
---
+++ octavvs/miccs/uitools.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 17:25:10 2020
+
+@author: carl
+"""
+
+import numpy as np
+
+from . import constants
+
+def ixfinder_nearest(wn, val):
+    return len(wn)-1 - (np.abs(wn - val)).argmin()
+
+def ixfinder_noless(wn, val):
+    return wn[::-1].searchsorted(val, 'left')
+
+def ixfinder_nomore(wn, val):
+    return wn[::-1].searchsorted(val, 'right')-1
+
+def slider_to_box(slider, box, wn, ixfinder):
+    "Set a QLineEdit from the value on a slider, discretized by the wavenumber array wn"
+    if wn is not None:
+        if (not box.hasAcceptableInput() or slider.value() != ixfinder(wn, box.value())):
+            box.setValue(wn[-1-slider.value()])
+    elif (not box.hasAcceptableInput() or
+            slider.value() != int(round(slider.maximum() *
+            (box.value() - constants.WMIN) / (constants.WMAX - constants.WMIN)))):
+        box.setValue(constants.WMIN + (constants.WMAX - constants.WMIN) *
+                     slider.value() / slider.maximum())
+
+def box_to_slider(slider, box, wn, ixfinder):
+    "Set a slider from the value in a lineedit, discretized by the wavenumber array wn"
+    if wn is not None:
+        if box.hasAcceptableInput():
+            slider.setValue(ixfinder(wn, box.value()))
+        else:
+            box.setValue(wn[-1-slider.value()])
+    else:
+        slider.setValue(int(round(slider.maximum() *
+              (box.value() - constants.WMIN) / (constants.WMAX - constants.WMIN))))
