cc21675707a8efcea20cbfe4dba92c2f78713508
Carl Troein <carl@thep.lu.se>
2021-01-26 20:54:12 +0100

    Bump version: 0.0.33 → 0.0.34

--- octavvs/ui/octavvsapplication.py
+++ octavvs/ui/octavvsapplication.py
@@ -26,7 +26,7 @@ class OctavvsMainWindow(QMainWindow):
 
     fileOptions = QFileDialog.Options() | QFileDialog.DontUseNativeDialog
 
-    octavvs_version = 'v0.0.33'
+    octavvs_version = 'v0.0.34'
 
     def __init__(self, parent=None):
         super().__init__(parent)
